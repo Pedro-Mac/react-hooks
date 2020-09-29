1137611c78e2ecc6b75fdd3f71b0f1550a313c7e
KozaX <55134615+KozaX@users.noreply.github.com>
2020-09-29 11:29:10 +0200

    add useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,9 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, {
+  /*useState,*/ useReducer,
+  useState,
+  useEffect,
+  useCallback
+} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,8 +11,23 @@ import ErrorModal from './../UI/ErrorModal';
 
 import Search from './Search';
 
+// !!! THIS IS NOT RELATED TO REDUX, BUT TO REACT USEREDUCER  !!!
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient];
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id);
+    default:
+      throw new Error('Should not get there');
+  }
+};
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
@@ -17,7 +37,8 @@ const Ingredients = () => {
 
   const handleFilteredIngredients = useCallback(
     filteredIngredients => {
-      setUserIngredients(filteredIngredients);
+      // setUserIngredients(filteredIngredients);
+      dispatch({ type: 'SET', ingredients: filteredIngredients });
     },
     [] /*if we pass an empty array, is as if we used ComponentDidMount*/
   );
@@ -34,42 +55,41 @@ const Ingredients = () => {
         return res.json();
       })
       .then(data => {
-        setUserIngredients(prevIngredients => [
-          ...prevIngredients,
-          { id: data.name, ...ingredient }
-        ]);
+        //   setUserIngredients(prevIngredients => [
+        //     ...prevIngredients,
+        //     { id: data.name, ...ingredient }
+        //   ]);
+        dispatch({ type: 'ADD', ingredient: { id: data.name, ...ingredient } });
       });
   };
 
   const removeIngredientHandler = ingredient => {
     setIsLoading(true);
     fetch(
-      `https://react-hooks-ef995.firebaseio.com/ingredients/${ingredient}.jon`,
+      `https://react-hooks-ef995.firebaseio.com/ingredients/${ingredient}.json`,
       {
         method: 'DELETE'
       }
     )
       .then(() => {
         setIsLoading(false);
-        setUserIngredients(prevIngredients => {
-          console.log('prevIngredients', prevIngredients);
-          const auxArray = prevIngredients.filter(value => {
-            console.log('filter value', value);
-            console.log('ingredient', ingredient);
-            return value.id !== ingredient;
-          });
-          console.log(auxArray);
-          return auxArray;
-        });
+        // setUserIngredients(prevIngredients => {
+        //   const auxArray = prevIngredients.filter(value => {
+        //     return value.id !== ingredient;
+        //   });
+
+        //   return auxArray;
+        // });
+        dispatch({ type: 'DELETE', id: ingredient });
       })
       .catch(err => {
         setError('Something went wrong');
+        setIsLoading(false);
       });
   };
 
   const clearError = () => {
     setError(null);
-    setIsLoading(false);
   };
 
   return (
